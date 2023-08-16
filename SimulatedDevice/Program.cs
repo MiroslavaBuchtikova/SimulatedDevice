3ebf2c1e21a9648c78bd7e082f249afe3b243755
Miroslava <miroslava.buchtikova@bluestep.se>
2023-08-16 18:19:43 +0200

    Initialization of console application which publish message

---
+++ SimulatedDevice/Program.cs
@@ -0,0 +1,24 @@
+﻿using SimulatedDevice;
+using DeviceCommunicationLibrary;
+using DeviceCommunicationLibrary.MessagePayloads;
+
+var publisher = new Publisher();
+
+var deviceInfo = new DeviceInfo
+{
+    DeviceId = Guid.NewGuid(),
+    Location = "BA main office",
+    Manufacturer = "Raspberry",
+    Model = "Raspberry Pi 4"
+};
+
+var magneticPayload = new WindowSensorPayload()
+{
+    IsWindowOpen = true,
+    BuildingName = "Building A",   
+    FloorNumber = 4,
+    WindowName = "Window 101",
+    OfficeName = "Office 201"
+};
+
+await publisher.SendMessageAsync(deviceInfo, magneticPayload);
